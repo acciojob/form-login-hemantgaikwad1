5492abf76f21465089a15efb436051af118281b1
Hemant Gaikwad <103812753+hemantgaikwad1@users.noreply.github.com>
2026-08-11 16:44:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+
+let form= document.querySelector('#form1');
+let inputs=form.querySelectorAll('input');
+
 function getFormvalue() {
     //Write your code here
-
+	let arr=[];
+	
+inputs.forEach((input)=>{
+	arr.push(input.value);
+})
+arr.pop();
+alert(arr.join(" "));
+	return;
 }
